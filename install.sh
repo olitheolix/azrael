6eac5eac28da4d2641c4d297441582f7469f2e35
Oliver Nagy <olitheolix@gmail.com>
2014-10-04 14:56:04 +1000

    Added 'install.sh' script.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install dependencies for Azrael core from Debian repos.
+apt-get install libassimp3 libassimp-dev python3-pymongo scons cython3 \
+python3-zmq libbullet-dev mongodb rabbitmq-server \
+python3-pip python3-numpy python3-pytest IPython3 \
+python3-tornado python3-pil
+
+# Install dependencies for Azrael core via PIP.
+pip3 install cytoolz setproctitle websocket-client==0.15
+
+# Compile the Cython code.
+cd azrael/bullet
+scons
+cd ../../
