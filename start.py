4089062860f311beabac6b60cb68d79b0f7d8e49
Oliver Nagy <olitheolix@gmail.com>
2014-10-06 11:02:08 +1100

    Re-arranged the cube grid upon startup.

--- start.py
+++ start.py
@@ -215,9 +215,9 @@ def spawnCubes(numRows, numCols, numLayers):
     # ----------------------------------------------------------------------
     # Two boosters, one left, one right. Both point in the same direction.
     b0 = parts.Booster(
-        partID=0, pos=[+0.005, 0, 0], direction=[0, 0, 1], max_force=1.0)
+        partID=0, pos=[+0.005, 0, 0], direction=[0, 0, 1], max_force=10.0)
     b1 = parts.Booster(
-        partID=1, pos=[-0.005, 0, 0], direction=[0, 0, 1], max_force=1.0)
+        partID=1, pos=[-0.005, 0, 0], direction=[0, 0, 1], max_force=10.0)
 
     # Two factories, one left one right. They will eject the new objects
     # forwards and backwards, respectively.
@@ -243,11 +243,11 @@ def spawnCubes(numRows, numCols, numLayers):
                 pos = np.array([col, row, lay], np.float64)
 
                 # Add some space in between the cubes.
-                pos *= 1 + spacing
+                pos *= -(1 + spacing)
 
                 # Center the positions.
-                pos[0] -= (numCols // 2) * (1 + spacing)
-                pos[1] -= (numRows // 2) * (1 + spacing)
+                pos[0] += (numCols // 2) * (1 + spacing)
+                pos[1] += (numRows // 2) * (1 + spacing)
                 pos[2] += 10
 
                 # Spawn the cube.
