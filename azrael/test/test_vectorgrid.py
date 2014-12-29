99f0adc71124d03e0bd7e2e0296b35d957f1dd7a
Oliver Nagy <olitheolix@gmail.com>
2014-12-29 15:25:24 +1100

    Added 'vectorgrid' module.

---
+++ azrael/test/test_vectorgrid.py
@@ -0,0 +1,329 @@
+import sys
+import time
+import pytest
+import IPython
+import subprocess
+import azrael.vectorgrid as vectorgrid
+import numpy as np
+
+ipshell = IPython.embed
+
+
+def test_set_get_single_invalid():
+    """
+    Call the set/get functions with invalid parameters and ensure they fail.
+    """
+    # Test parameters.
+    vg = vectorgrid
+    pos = np.array([1, 2, 3], np.float64)
+    value = np.array([-1, 0, 1], np.float64)
+
+    # Delete all grids used in this test.
+    assert vg.deleteAllGrids().ok
+
+    # Attempt to get the value from a non-existing grid.
+    ret = vg.getValue('force', pos)
+    assert not ret.ok
+    assert 'unknown grid' in ret.msg.lower()
+    assert ret.data is None
+    assert ret == vg.setValue('force', pos, value)
+
+    # Invalid arguments to 'getValue'.
+    assert not vg.getValue('force', np.array([1, 2])).ok
+
+    # Invalid arguments to 'setValue'.
+    assert not vg.setValue('force', np.array([1, 2]), value).ok
+    assert not vg.setValue('force', pos, np.array([1, 2])).ok
+    assert not vg.setValue('force', pos, np.array([1, 2, 3, 4])).ok
+
+    print('Test passed')
+
+
+def test_set_get_single():
+    """
+    Define a new grid type and set/get some values.
+    """
+    # Test parameters.
+    vg = vectorgrid
+    pos = np.array([1, 2, 3], np.float64)
+    value = np.array([-1, 0, 1], np.float64)
+    name = 'force'
+
+    # Delete all grids used in this test.
+    assert vg.deleteAllGrids().ok
+
+    # Attempt to set/get values of an undefined grid.
+    assert not vg.getValue(name, pos).ok
+
+    # Attempt to set the value of a non-existing grid.
+    assert not vg.setValue(name, pos, value).ok
+
+    # Define a new grid. Its name is "force", it consists of 3-element vectors,
+    # and has a spatial granularity of 1m in each dimension.
+    assert vg.defineGrid(name=name, elDim=3, granularity=1).ok
+
+    # Query a value. This must return a zero vector (3 elements) because this
+    # is the default value of the grid.
+    ret = vg.getValue(name, pos)
+    assert ret.ok
+    assert np.array_equal(ret.data, np.zeros(3, np.float64))
+
+    # Repeat at a different position.
+    ret = vg.getValue(name, pos + np.pi)
+    assert ret.ok
+    assert np.array_equal(ret.data, np.zeros(3, np.float64))
+
+    # Update the value and query it once more.
+    assert vg.setValue(name, pos, value).ok
+    ret = vg.getValue(name, pos)
+    assert ret.ok
+    assert np.array_equal(ret.data, value)
+
+    print('Test passed')
+
+
+def test_define_reset_delete_grid():
+    """
+    Define a new grid, add/set a value, delete the grid, and ensure it cannot
+    be accessed anymore.
+    """
+    # Test parameters.
+    vg = vectorgrid
+    pos = np.array([1, 2, 3], np.float64)
+    value = np.array([-1, 0, 1], np.float64)
+    name = 'force'
+
+    # Delete all grids used in this test.
+    assert vg.deleteAllGrids().ok
+
+    # Attempt to access the undefined grid.
+    assert not vg.getValue(name, pos).ok
+    assert not vg.setValue(name, pos, value).ok
+
+    # Define a new grid.
+    assert vg.defineGrid(name=name, elDim=3, granularity=1).ok
+
+    # Query default value.
+    ret = vg.getValue(name, pos)
+    assert ret.ok and np.array_equal(ret.data, np.zeros(3))
+
+    # Update the value at 'pos'.
+    assert vg.setValue(name, pos, value).ok
+    assert np.array_equal(vg.getValue(name, pos).data, value)
+
+    # Reset the grid and query 'pos' again.
+    assert vg.resetGrid(name).ok
+    assert np.array_equal(vg.getValue(name, pos).data, np.zeros(3))
+
+    # Delete the grid and ensure it has become inaccessible.
+    assert vg.getValue(name, pos).ok
+    assert vg.setValue(name, pos, value).ok
+    assert vg.deleteGrid(name).ok
+    assert not vg.getValue(name, pos).ok
+    assert not vg.setValue(name, pos, value).ok
+
+    print('Test passed')
+
+
+def test_define_reset_delete_grid_invalid():
+    """
+    Test invalid calling signatures.
+    """
+    # Test parameters.
+    vg = vectorgrid
+    pos = np.array([1, 2, 3], np.float64)
+    value = np.array([-1, 0, 1], np.float64)
+    name = 'force'
+
+    # Delete all grids used in this test.
+    assert vg.deleteAllGrids().ok
+
+    # Attempt to reset a non-existing grid.
+    ret = vg.resetGrid('blah')
+    assert not ret.ok
+    assert 'unknown grid' in ret.msg.lower()
+
+    # Attempt to delete a non-existing grid.
+    ret = vg.deleteGrid('blah')
+    assert not ret.ok
+    assert 'unknown grid' in ret.msg.lower()
+
+    # Attempt to define a grid with non-positive granularity.
+    assert not vg.defineGrid(name=name, elDim=3, granularity=-1).ok
+    assert not vg.defineGrid(name=name, elDim=3, granularity=0).ok
+
+    # Attempt to define a grid with invalid elDim value.
+    assert not vg.defineGrid(name=name, elDim=-1, granularity=1).ok
+    assert not vg.defineGrid(name=name, elDim=0, granularity=1).ok
+
+    # Attempt to define the same grid twice.
+    assert vg.defineGrid(name=name, elDim=3, granularity=1).ok
+    ret = vg.defineGrid(name=name, elDim=3, granularity=1)
+    assert not ret.ok
+    assert 'already exists' in ret.msg.lower()
+
+    print('Test passed')
+
+
+def test_set_get_region():
+    """
+    Set/get multiple values at once.
+    """
+    # Test parameters.
+    vg = vectorgrid
+    ofs = np.zeros(3, np.float64)
+    elDim = 3
+    regionDim = np.array([2, 4, 6], np.int64)
+    dataDim = np.hstack((regionDim, elDim))
+    name = 'force'
+
+    # Delete all grids used in this test.
+    assert vg.deleteAllGrids().ok
+
+    # Define a new grid.
+    assert vg.defineGrid(name=name, elDim=elDim, granularity=1).ok
+
+    # Query an entire region.
+    ret = vg.getRegion(name, ofs, regionDim)
+    assert ret.ok
+    assert np.array_equal(ret.data, np.zeros(dataDim, np.float64))
+
+    # Create the data set for an `elDim` vector field. For instance, an EM
+    # field is 3D vector field (ie elDim=3), that is at position (x,y,z) has an
+    # associated (E_x, E_y, E_z) vector.
+    data = np.zeros(dataDim, np.float64)
+    val = 0
+    for x in range(regionDim[0]):
+        for y in range(regionDim[1]):
+            for z in range(regionDim[2]):
+                data[x, y, z, :] = val
+                val += 1
+    del val, x, y, z
+
+    # Apply the data set.
+    ret = vg.setRegion(name, ofs, data)
+    assert ret.ok
+
+    # Query the entire region.
+    ret = vg.getRegion(name, ofs, regionDim)
+    assert np.array_equal(ret.data, data)
+
+    # Query all points individually.
+    for x in range(regionDim[0]):
+        for y in range(regionDim[1]):
+            for z in range(regionDim[2]):
+                # Use single query.
+                pos = ofs + np.array([x, y, z], np.float64)
+                ret = vg.getValue(name, pos)
+                assert ret.ok
+                assert np.array_equal(ret.data, data[x, y, z])
+
+                # Use region query.
+                ret = vg.getRegion(name, np.array(pos), (1, 1, 1))
+                assert ret.ok
+                assert ret.data.shape == (1, 1, 1, elDim)
+                assert np.array_equal(ret.data[0, 0, 0], data[x, y, z])
+
+    # Query other subsets.
+    ret = vg.getRegion(name, ofs, regionDim - 1)
+    assert ret.ok
+    assert np.array_equal(ret.data.shape[:3], regionDim - 1)
+    assert np.array_equal(ret.data, data[:-1, :-1, :-1])
+
+    ret = vg.getRegion(name, ofs + 1, regionDim - 1)
+    assert ret.ok
+    assert np.array_equal(ret.data.shape[:3], regionDim - 1)
+    assert np.array_equal(ret.data, data[1:, 1:, 1:])
+
+    print('Test passed')
+
+
+def test_deleteAll():
+    """
+    Define a few grids and delete them all at once with 'deleteAll'.
+    """
+    # Test parameters.
+    vg = vectorgrid
+
+    # Remove any pending grids.
+    assert vg.deleteAllGrids().ok
+
+    # No grids must exist.
+    ret = vg.getAllGridNames()
+    assert ret.ok
+    assert ret.data == tuple()
+
+    # The 'deleteAllGrids' function must succeed (even though it will not
+    # delete any grids because none have been defined yet).
+    assert vg.deleteAllGrids().ok
+
+    # Define one grid and delete it with 'deleteAllGrids'.
+    assert vg.defineGrid('grid_1', 3, 1).ok
+    assert vg.getAllGridNames().data == ('grid_1', )
+    assert vg.deleteAllGrids().ok
+    assert vg.getAllGridNames().data == tuple()
+
+    # Define two grids and delete both with 'deleteAllGrids'.
+    assert vg.defineGrid('grid_1', 3, 1).ok
+    assert vg.defineGrid('grid_2', 3, 1).ok
+    assert set(vg.getAllGridNames().data) == set(['grid_1', 'grid_2'])
+    assert vg.deleteAllGrids().ok
+    assert vg.getAllGridNames().data == tuple()
+
+    print('Test passed')
+
+
+def test_granularity():
+    """
+    Use a grid with granularity that is not 1.
+    """
+    # Test parameters.
+    vg = vectorgrid
+    pos = np.array([1, 2, 3], np.float64)
+    value = np.array([-1, 0, 1], np.float64)
+    gran = 0.5
+    elDim = 3
+    name = 'force'
+
+    # Delete all grids used in this test.
+    assert vg.deleteAllGrids().ok
+
+    # Define a new grid.
+    assert vg.defineGrid(name=name, elDim=elDim, granularity=gran).ok
+
+    # Query default value at 'pos'.
+    ret = vg.getValue(name, pos)
+    assert ret.ok and np.array_equal(ret.data, np.zeros(elDim))
+
+    # Update the value at 'pos'.
+    assert vg.setValue(name, pos, value).ok
+    assert np.array_equal(vg.getValue(name, pos).data, value)
+
+    # Query it again at 'pos'.
+    ret = vg.getValue(name, pos)
+    assert ret.ok and np.array_equal(ret.data, value)
+
+    # Now query the values near pos. Internally, the vectorgrid engine will use
+    # integer truncation (not rounding) to determine which vector element to
+    # actually retrieve.
+    ret = vg.getValue(name, pos + gran / 2)
+    assert ret.ok and np.array_equal(ret.data, value)
+
+    ret = vg.getValue(name, pos + gran)
+    assert ret.ok and np.array_equal(ret.data, np.zeros(elDim))
+
+    ret = vg.getValue(name, pos - 0.1)
+    assert ret.ok and np.array_equal(ret.data, np.zeros(elDim))
+
+    print('Test passed')
+
+
+if __name__ == '__main__':
+    test_granularity()
+
+    test_deleteAll()
+    test_define_reset_delete_grid()
+    test_define_reset_delete_grid_invalid()
+    test_set_get_single()
+    test_set_get_single_invalid()
+    test_set_get_region()
---
+++ azrael/vectorgrid.py
@@ -0,0 +1,327 @@
+# Copyright 2014, Oliver Nagy <olitheolix@gmail.com>
+#
+# This file is part of Azrael (https://github.com/olitheolix/azrael)
+#
+# Azrael is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Azrael is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Azrael. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Vector grid engine.
+
+All grids extend to infinity with a default value of zero. The basic usage
+pattern is to define a new grid and then set/query values on it.
+
+All grids have a spatial granularity. It is possible to set/query values at any
+(floating point) position but the set/get functions will always round it to the
+nearest granularity multiple.
+
+Internally, the engine only adds non-zero values to the database, and removes
+all those set to zero.
+"""
+import sys
+import logging
+import pymongo
+import IPython
+import numpy as np
+import azrael.util as util
+import azrael.config as config
+import azrael.bullet.bullet_data as bullet_data
+
+from collections import namedtuple
+from azrael.typecheck import typecheck
+
+ipshell = IPython.embed
+
+# Global database handles.
+_DB_Grid = None
+
+
+# Work package related.
+RetVal = namedtuple('RetVal', 'ok msg data')
+
+
+# Create module logger.
+logit = logging.getLogger('azrael.' + __name__)
+
+
+def deleteAllGrids():
+    """
+    Delete all currently defined grids.
+
+    :return: None
+    """
+    global _DB_Grid
+    client = pymongo.MongoClient()
+    name = 'azrael_grid'
+    client.drop_database(name)
+    _DB_Grid = client[name]
+
+    return RetVal(True, None, None)
+
+
+def getAllGridNames():
+    """
+    Return all the names of all currently defined grids.
+
+    :return: grid names.
+    :rtype: tuple of strings.
+    """
+    if _DB_Grid is None:
+        return RetVal(False, 'Not initialised', None)
+    else:
+        # Every grid sits in its own collection. The grid names are hence the
+        # grid names save 'system.indexes' which Mongo creates internally
+        # itself.
+        names = set(_DB_Grid.collection_names())
+        names.discard('system.indexes')
+        return RetVal(True, None, tuple(names))
+
+
+@typecheck
+def defineGrid(name: str, elDim: int, granularity: (int, float)):
+    """
+    Define a new grid with ``name``.
+
+    Every element of the grid is a vector with ``elDim`` elements. The grid has
+    the spatial ``granularity`` (in meters). The minimum granularity is 1E-9m.
+
+    :param str name: grid name
+    :param int elDim: number of data dimensions.
+    :param float granularity: spatial granularity in Meters.
+    :return: None
+    """
+    # DB handle must have been initialised.
+    if _DB_Grid is None:
+        return RetVal(False, 'Not initialised', None)
+
+    # Sanity check.
+    if granularity < 1E-9:
+        return RetVal(False, 'Granularity must be >1E-9', None)
+
+    # Sanity check.
+    if elDim <= 0:
+        return RetVal(False, 'Vector dimension must be positive integer', None)
+
+    # Return with an error if the grid ``name`` is already defined.
+    if name in _DB_Grid.collection_names():
+        msg = 'Grid <{}> already exists'.format(name)
+        logit.info(msg)
+        return RetVal(False, msg, None)
+
+    # Flush the DB (just a pre-caution) and add the admin element.
+    db = _DB_Grid[name]
+    db.drop()
+    db.insert({'admin': 'admin', 'elDim': elDim, 'gran': granularity})
+
+    # All good.
+    return RetVal(True, None, None)
+
+
+@typecheck
+def resetGrid(name: str):
+    """
+    Reset all values of the grid ``name``.
+
+    :param str name: grid name to reset.
+    :return: None
+    """
+    # DB handle must have been initialised.
+    if _DB_Grid is None:
+        return RetVal(False, 'Not initialised', None)
+
+    # Return with an error if the grid ``name`` does not exist.
+    if name not in _DB_Grid.collection_names():
+        msg = 'Unknown grid <{}>'.format(name)
+        logit.info(msg)
+        return RetVal(False, msg, None)
+
+    # Ensure the admin element exists.
+    db = _DB_Grid[name]
+    admin = db.find_one({'admin': 'admin'})
+    if admin is None:
+        return RetVal(False, 'Bug: could not find admin element', None)
+
+    # Resetting a grid equates to deleting all values in the collection so that
+    # all values assume their default again. We therefore simply drop the
+    # entire collection and re-insert the admin element.
+    db.drop()
+    db.insert(admin)
+    return RetVal(True, None, None)
+
+
+@typecheck
+def deleteGrid(name: str):
+    """
+    Delete the grid ``name``.
+
+    :param str name: grid name.
+    :return: None
+    """
+    # DB handle must have been initialised.
+    if _DB_Grid is None:
+        return RetVal(False, 'Not initialised', None)
+
+    # Return with an error if the grid ``name`` does not exist.
+    if name not in _DB_Grid.collection_names():
+        msg = 'Unknown grid <{}>'.format(name)
+        logit.info(msg)
+        return RetVal(False, msg, None)
+
+    # Flush the collection and insert the admin element again.
+    _DB_Grid.drop_collection(name)
+
+    # All good.
+    return RetVal(True, None, None)
+
+
+@typecheck
+def getValue(name: str, pos: np.ndarray):
+    """
+    Return the value at ``pos``.
+
+    :param str name: grid name
+    :param 3D-vec pos: position in grid
+    :return: data vector at ``pos``.
+    """
+    # Leverage 'getRegion' to do the actual work.
+    ret = getRegion(name, pos, (1, 1, 1))
+    if ret.ok:
+        return RetVal(True, None, ret.data[0, 0, 0])
+    else:
+        return ret
+
+
+@typecheck
+def setValue(name: str, pos: np.ndarray, value: np.ndarray):
+    """
+    Update the value at ``pos`` to ``value``.
+
+    :param str name: grid name
+    :param 3D-vec pos: position in grid
+    :param vector value: the value to insert at ``pos``.
+    :return: None
+    """
+    # Leverage 'setRegion' to do the actual work. Wrap the ``value`` into a
+    # 4-D data structure (the first three denote the position, the fourth holds
+    # the actual value).
+    tmp = np.zeros((1, 1, 1, len(value)), np.float64)
+    tmp[0, 0, 0] = value
+    return setRegion(name, pos, tmp)
+
+
+@typecheck
+def getRegion(name: str, ofs: np.ndarray,
+              regionDim: (np.ndarray, list, tuple)):
+    """
+    Return the grid values starting at ``ofs``.
+
+    The dimension of the returned depends on ``regionDim`` and the elDim
+    parameter used to define the grid. For instance, if regionDim=(1, 2, 3) and
+    the elDim=4, then the shape of the returned NumPy array is (1, 2, 3, 4).
+
+    :param str name: grid name.
+    :param 3D-vector ofs: start position in grid from where to read values.
+    :param 3D-vector regionDim: number of values to read in each dimension.
+    :return: 4D matrix.
+    """
+    # Return with an error if the grid ``name`` does not exist.
+    if name not in _DB_Grid.collection_names():
+        msg = 'Unknown grid <{}>'.format(name)
+        logit.info(msg)
+        return RetVal(False, msg, None)
+    else:
+        db = _DB_Grid[name]
+
+    # Ensure ``ofs`` and ``regionDim`` have the correct size.
+    if (len(ofs) != 3) or (len(regionDim) != 3):
+        return RetVal(False, 'Invalid parameter values', None)
+
+    # Ensure the region dimensions are positive integers.
+    regionDim = np.array(regionDim, np.int64)
+    if np.amin(regionDim) < 1:
+        return RetVal(False, 'Dimensions must be positive', None)
+
+    # Retrieve the admin field for later use.
+    admin = db.find_one({'admin': 'admin'})
+    if admin is None:
+        return RetVal(False, 'Bug: could not find admin element', None)
+
+    # Allocate the output array.
+    out = np.zeros(np.hstack((regionDim, admin['elDim'])), np.float64)
+
+    # Populate the output array.
+    for x in range(regionDim[0]):
+        for y in range(regionDim[1]):
+            for z in range(regionDim[2]):
+                # Fetch the vector field at the corresponding position.
+                pos = ofs + np.array([x, y, z])
+
+                # Convert the (floating point) position to an integer according
+                # to the grid granularity.
+                pos = (pos / admin['gran']).astype(np.int64)
+                doc = db.find_one({'pos': pos.tolist()})
+
+                # Populate the output data structure.
+                if doc is not None:
+                    out[x, y, z, :] = np.array(doc['val'], np.float64)
+
+    return RetVal(True, None, out)
+
+
+@typecheck
+def setRegion(name: str, ofs: np.ndarray, value: np.ndarray):
+    """
+    Update the grid values starting at ``ofs`` with ``value``.
+
+    :param str name: grid name.
+    :param 3D-vector ofs: start position in grid from where to read values.
+    :param 4D-vector value: the data values.
+    :return: None
+    """
+    # Return with an error if the grid ``name`` does not exist.
+    if name not in _DB_Grid.collection_names():
+        msg = 'Unknown grid <{}>'.format(name)
+        logit.info(msg)
+        return RetVal(False, msg, None)
+    else:
+        db = _DB_Grid[name]
+
+    # Ensure ``ofs`` has the correct size.
+    if len(ofs) != 3:
+        return RetVal(False, 'Invalid parameter values', None)
+
+    # Retrieve the admin field for later use.
+    admin = db.find_one({'admin': 'admin'})
+    if admin is None:
+        return RetVal(False, 'Bug: could not find admin element', None)
+
+    # Ensure ``value`` has the correct number of dimensions. To elaborate,
+    # every ``value`` needs at least 3 spatial dimensions, plus an additional
+    # dimension that holds the actual data vector, which must have length
+    # ``elDim``.
+    if (len(value.shape) != 4) or (value.shape[3] != admin['elDim']):
+        return RetVal(False, 'Invalid data dimension', None)
+
+    # Populate the output array.
+    for x in range(value.shape[0]):
+        for y in range(value.shape[1]):
+            for z in range(value.shape[2]):
+                pos = ofs + np.array([x, y, z])
+                pos = (pos / admin['gran']).astype(np.int64)
+                pos = pos.tolist()
+                val = value[x, y, z, :].tolist()
+                ret = db.update({'pos': pos},
+                                {'pos': pos, 'val': val},
+                                upsert=True)
+
+    return RetVal(True, None, None)
