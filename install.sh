c0a4db6e380922a47f41c0cdd4f677164ed88336
Oliver Nagy <olitheolix@gmail.com>
2015-02-08 17:28:40 +1100

    Updated install file.

--- install.sh
+++ install.sh
@@ -2,23 +2,22 @@
 
 # (K)Ubuntu repos.
 apt-get install -y \
+  git \
   IPython3 \
   libassimp-dev \
   libassimp3 \
   libboost-python-dev \
-  mongodb \
   python3-matplotlib \
   python3-netifaces \
   python3-numpy \
   python3-pandas \
   python3-pil \
   python3-pip \
-  python3-pymongo \
   python3-tornado \
   python3-zmq
 
 # PIP repos.
-pip3 install cytoolz setproctitle websocket-client==0.15 pytest pytest-cov
+pip3 install cytoolz setproctitle websocket-client==0.15 pymongo pytest-cov
 
 # PIP install boost-python-wrapper directory from GitHub.
 pip3 install --install-option="-j" \
