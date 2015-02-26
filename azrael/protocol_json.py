fcd28ad9ddab88c80ff0e74e1fd1659db0dbda93
Oliver Nagy <olitheolix@gmail.com>
2015-02-26 19:26:28 +1100

    Removed special handling for 'bytes' from JSON encoder.

--- azrael/protocol_json.py
+++ azrael/protocol_json.py
@@ -32,8 +32,6 @@ class AzraelEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
-        if isinstance(obj, bytes):
-            return list(obj)
         return json.JSONEncoder.default(self, obj)
 
 
