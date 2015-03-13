abc7b60ba046c49398edf01f65bf92c51404f0a8
Oliver Nagy <olitheolix@gmail.com>
2015-03-13 15:59:09 +1100

    Updated version number in Sphinx documentation to v0.2.

--- doc/conf.py
+++ doc/conf.py
@@ -61,7 +61,7 @@ copyright = '2015, Oliver Nagy'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
