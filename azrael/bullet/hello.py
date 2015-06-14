e44d7f4fb71029f334505fe983da1ca59d8b1199
Oliver Nagy <olitheolix@gmail.com>
2015-06-14 15:10:22 +1000

    bugfix: the Bullet demo 'hello.py' now runs with latest version.

--- azrael/bullet/hello.py
+++ azrael/bullet/hello.py
@@ -4,19 +4,19 @@ Python version of Bullet's own 'Hello World' demo using 'azBullet' wrapper.
 import azBullet
 from azBullet import Vec3, Quaternion
 from azBullet import StaticPlaneShape, SphereShape
-from azBullet import DefaultRigidBodyState, Transform
+from azBullet import DefaultMotionState, Transform
 from azBullet import RigidBody, RigidBodyConstructionInfo
 
 sim = azBullet.BulletBase()
-sim.setGravity(0, -10, 0)
+sim.setGravity(Vec3(0, -10, 0))
 
 # Create a collision shape for the Ball and for the Ground.
 ballShape = SphereShape(1)
 groundShape = StaticPlaneShape(Vec3(0, 1, 0), 1)
 
-# Create a Rigid body for the Ground based on the collision shape.
+# Create a Rigid Body for the Ground based on the collision shape.
 mass = 0
-groundRigidBodyState = DefaultRigidBodyState(
+groundRigidBodyState = DefaultMotionState(
     Transform(Quaternion(0, 0, 0, 1), Vec3(0, -1, 0)))
 ci = RigidBodyConstructionInfo(mass, groundRigidBodyState, groundShape)
 groundRigidBody = RigidBody(ci)
@@ -26,7 +26,7 @@ del mass, ci
 
 # Create a Rigid body for the Ball based on its collision shape. Let Bullet do
 # the heavy lifting and compute the mass and inertia for us.
-fallRigidBodyState = DefaultRigidBodyState(
+fallRigidBodyState = DefaultMotionState(
     Transform(Quaternion(0, 0, 0, 1), Vec3(0, 20, 0)))
 mass = 1
 fallInertia = ballShape.calculateLocalInertia(mass)
@@ -39,7 +39,7 @@ del mass, fallInertia, ci
 # Step the simulation and print the position of the ball.
 for ii in range(10):
     sim.stepSimulation(0.5, 30)
-    ms = fallRigidBody.getRigidBodyState()
+    ms = fallRigidBody.getMotionState()
     wt = ms.getWorldTransform()
     print(wt.getOrigin())
 
