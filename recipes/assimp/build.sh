4cd75d07f60e9015ec33b35b73ffb07244e17b64
Oliver Nagy <olitheolix@gmail.com>
2015-10-24 21:36:23 +1100

    Added Anaconda recipes for AssImp library.

---
+++ recipes/assimp/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#export INCLUDE_PATH=$PREFIX/include
+#export LIBRARY_PATH=$PREFIX/lib
+
+# Build the AssImp library.
+cmake \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
+    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++
+
+make install
+
+# Install the Python bindings.
+cd port/PyAssimp
+python setup.py install
+
+# Run the test suite (not really a test suite but will at least verify
+# that the AssImp library is found by PyAssImp).
+cd scripts
+python quicktest.py
