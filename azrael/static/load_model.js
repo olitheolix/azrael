b0badbbabda12f5af4d9bbb255da77da06de30a9
Oliver Nagy <olitheolix@gmail.com>
2015-11-02 17:21:53 +1100

    Webviewer can now load models in ThreeJS JSON format.

--- azrael/static/load_model.js
+++ azrael/static/load_model.js
@@ -1,3 +1,8 @@
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+
 onmessage = function (e) {
     // Status message.
     console.log('<load_model.js> Worker received request to download '
@@ -22,9 +27,24 @@ onmessage = function (e) {
             url = e.data.baseURL + obj[frag_name]['url_frag']
             switch (obj[frag_name].fragtype) {
             case 'RAW':
+                // Find the first file that ends in JSON. Consider it
+                // an error if no such file exists.
+                var modelname = null
+                for (var idx in obj[frag_name].files) {
+                    var fname = obj[frag_name].files[idx]
+                    if (!endsWith(fname.toLowerCase(fname), '.json')) continue;
+                    modelname = fname;
+                    break
+                }
+                if (modelname == null) {
+                    console.log('Worker: did not find RAW json file')
+                    model = {'type': null};
+                    break
+                }
+
                 // All raw fragments are stored in a JSON file on the
                 // server called 'model.json'. Download it.
-                xmlhttp.open("GET", url + '/model.json', false);
+                xmlhttp.open("GET", url + '/' + modelname, false);
                 xmlhttp.send();
                 try {
                     model = JSON.parse(xmlhttp.responseText);
@@ -38,8 +58,22 @@ onmessage = function (e) {
             case 'DAE':
                 model = {'type': 'DAE', 'url': url + '/' + frag_name};
                 break;
-            case 'OBJ':
-                model = {'type': 'OBJ', 'url': url + '/' + frag_name + '.json'};
+            case '3JS_V4':
+                // Find the first file that ends in JSON. Consider it
+                // an error if no such file exists.
+                model = null
+                for (var idx in obj[frag_name].files) {
+                    var fname = obj[frag_name].files[idx]
+                    if (!endsWith(fname.toLowerCase(fname), '.json')) continue;
+
+                    model = {'type': '3JS_V4', 'url': url + '/' + fname};
+                    break
+                }
+                if (model == null) {
+                    console.log('Worker: did not find 3JS_v4 JSON file')
+                    model = {'type': null};
+                    break
+                }
                 break;
             default:
                 model = {'type': null};
