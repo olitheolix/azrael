ad3a324c4762a28136c1d09d637f275de42ba32e
Oliver Nagy <olitheolix@gmail.com>
2015-11-27 13:28:28 +1100

    Package initialisation now relies on 'datastore.init()' instead of using custom code.

--- azrael/__init__.py
+++ azrael/__init__.py
@@ -16,43 +16,33 @@
 # along with Azrael. If not, see <http://www.gnu.org/licenses/>.
 
 import time
-import pymongo
 import multiprocessing
-import azrael.config as config
+import azrael.datastore
 
 # Use 'fork' system call to create new processes.
 multiprocessing.set_start_method('fork')
 
 
-def waitForDatabase(timeout=60):
+def waitForDatabases(timeout=60):
     """
-    Raise `ImportError` if we cannot reach MongoDB within ``timeout`` seconds.
+    Raise `ImportError` if data stores are unavailable for ``timeout`` seconds.
 
-    Azrael crucially relies on MongoDB. The main purpose of this function is to
-    ensure it is ready. However, MongoDB may need time to start up because it
-    eg runs in a different container, started slower than Azrael, ...
+    Azrael crucially relies on the databases (via the Datastore API). The main
+    purpose of this function is to wait until they are ready. This is not a
+    problem locally but if the databases are started separately (eg in Docker
+    containers) then it may take a while until they accept connections.
     """
-    def isMongoLive():
-        """
-        Return *True* if connecting to MongoDB is possible.
-        """
-        try:
-            config.getMongoClient()
-        except pymongo.errors.ConnectionFailure:
-            return False
-        return True
-
-    # Attempt to reach MongoDB for `timeout` seconds before giving up.
+    # Attempt to initialise the data stores.
     t0 = time.time()
-    while not isMongoLive():
+    while azrael.datastore.init(flush=True) is False:
         if time.time() - t0 > timeout:
             # A minute has passed - abort with an error.
             print('failed!')
-            print('Could not connect to MongoDB -- Abort')
-            raise ImportError('Could not connect to MongoDB')
+            print('Could not reach all Datastore backends -- Abort')
+            raise ImportError('Could not connect to databases')
         time.sleep(1)
         print('.', end='', flush=True)
 
 
-# Do not proceed with Azrael until MongoDB is accessible.
-waitForDatabase(timeout=60)
+# Do not proceed with Azrael until the databases are ready.
+waitForDatabases(timeout=60)
