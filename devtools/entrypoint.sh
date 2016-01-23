e09d555605e4d02f930787b23d161b81ad420429
Oliver Nagy <olitheolix@gmail.com>
2016-01-23 13:01:03 +1100

    Docu update.

--- devtools/entrypoint.sh
+++ devtools/entrypoint.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 #
-# Accept the name of a demo and spawn it with reasonable default
-# arguments. This script is not intended to be used outside the
-# Azrael/Docker container.
+# Translate mnemonic names for demos and services to the
+# corresponding python command with the correct arguments.
+# 
+# If the first argument is not a recognised mnemonic then execute the
+# specified command verbatim.
+# 
+# Note: this script will have PID 1 and is thus the only process to
+# which Docker will send the SIGTERM signal upon shutdown. To ensure
+# the signal is propagates to the Python program it is paramount to
+# start them with the 'exec' keyword. This will replace this very
+# process with a new one without changing the PID.
+#
+# A good article on the intricacies of when and how Docker sends
+# signals is here:
+# https://www.ctl.io/developers/blog/post/gracefully-stopping-docker-containers
 
 # Abort if we are not inside an AZrael/Docker container.
 if [ -z $INSIDEDOCKER ]; then
